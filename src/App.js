4cdbac286864506424ec3b446567929eed55bfdb
Tyler Weeres <tweeres04@gmail.com>
2019-10-31 21:51:08 -0700

    Jeopardy!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import update from 'immutability-helper';
+import classnames from 'classnames';
+
 import './App.css';
 
+import questionData from './questionData';
+
+const initialQuestions = questionData.map(c => ({
+	...c,
+	questions: c.questions.map(q => ({ ...q, answered: false }))
+}));
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [questions, setQuestions] = useState(initialQuestions);
+	const [currentQuestion, setCurrentQuestion] = useState();
+
+	function setAnswered(question) {
+		const categoryIndex = questions.findIndex(c =>
+			c.questions.some(q => q === question)
+		);
+		const questionIndex = questions[categoryIndex].questions.findIndex(
+			q => q === question
+		);
+		setQuestions(
+			update(questions, {
+				[categoryIndex]: {
+					questions: {
+						[questionIndex]: {
+							answered: {
+								$set: true
+							}
+						}
+					}
+				}
+			})
+		);
+	}
+
+	return (
+		<>
+			<div className="categories">
+				{questions.map(({ name, questions }, ci) => (
+					<div className="category child-borders child-shadows" key={ci}>
+						<div className="category-name margin padding">{name}</div>
+						{questions.map((q, qi) => (
+							<div
+								key={qi}
+								className={classnames('question margin padding', {
+									answered: q.answered
+								})}
+								onClick={
+									q.answered
+										? () => {}
+										: () => {
+												setCurrentQuestion(q);
+										  }
+								}
+							>
+								{q.answered || `$${(qi + 1) * 100}`}
+							</div>
+						))}
+					</div>
+				))}
+			</div>
+			<input
+				className="modal-state"
+				id="modal-1"
+				type="checkbox"
+				checked={currentQuestion}
+			/>
+			{currentQuestion && (
+				<QuestionModal
+					currentQuestion={currentQuestion}
+					setCurrentQuestion={setCurrentQuestion}
+					setAnswered={setAnswered}
+				/>
+			)}
+		</>
+	);
+}
+
+function QuestionModal({ currentQuestion, setCurrentQuestion, setAnswered }) {
+	const [flipped, setFlipped] = useState(false);
+
+	const { question, answer } = currentQuestion;
+	return (
+		<div className="modal">
+			<div
+				className="modal-bg"
+				for="modal-1"
+				onClick={() => {
+					setCurrentQuestion(null);
+				}}
+			></div>
+			<div className="modal-body" for="modal-1">
+				{flipped ? answer : question}
+				{flipped || (
+					<div className="margin-top">
+						<button
+							onClick={() => {
+								setAnswered(currentQuestion);
+								setFlipped(!flipped);
+							}}
+						>
+							Flip
+						</button>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/questionData.js
@@ -0,0 +1,55 @@
+module.exports = [
+	{
+		name: 'Susan History',
+		questions: [
+			{
+				question:
+					'This individual is the first ever offical susan goal for hte 2019-2020 season',
+				answer: 'Who is Claire Ethier?'
+			},
+			{
+				question:
+					'This player is considered one of the "Founding Fathers" of the original GHOOD',
+				answer: 'Who is Catherine Cullen?'
+			}
+		]
+	},
+	{
+		name: "Women's Socker - World Stage",
+		questions: [
+			{
+				question:
+					'This individual is crowned a HERO at the infamous 2012 Olympics scoring a hat trick in one of the most epic battles in Canadian Soccer history.',
+				answer: 'Who is Christine Sinclair?'
+			}
+		]
+	},
+	{
+		name: 'Taylor Swift',
+		questions: [
+			{
+				question: 'October 24, 2006 was the realse date of this album',
+				answer: 'What is Taylor Swift?'
+			},
+			{
+				question:
+					'Three Susan members were spotted in the pit during this album tour in 2013',
+				answer: 'What is Red?'
+			},
+			{
+				question:
+					'This Susan individual sent Melissa her *first ever* Taylor Swift song back in 2006',
+				answer: 'Who is Kelsey Blake?'
+			}
+		]
+	},
+	{
+		name: 'Facts About Cows',
+		questions: [
+			{
+				question: 'Female cows are called cows. Male cows are called ________',
+				answer: 'What are bulls?'
+			}
+		]
+	}
+];
