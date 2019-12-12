4b3e6b214734765baaa3b9ff02f60bd7ccfff199
Tyler Weeres <tweeres04@gmail.com>
2019-12-11 22:13:59 -0800

    More question data

--- src/questionData.js
+++ src/questionData.js
@@ -4,6 +4,11 @@ module.exports = [
 		questions: [
 			{
 				question:
+					'In the past decade, this player was the youngest player to sign with at the age of 15 SAANICH FUSION in September 2008 (Formally known as GHOOD).',
+				answer: 'Who is Sarah Douglas?'
+			},
+			{
+				question:
 					'This individual is the first ever offical susan goal for hte 2019-2020 season',
 				answer: 'Who is Claire Ethier?'
 			},
@@ -11,6 +16,16 @@ module.exports = [
 				question:
 					'This player is considered one of the "Founding Fathers" of the original GHOOD',
 				answer: 'Who is Catherine Cullen?'
+			},
+			{
+				question:
+					'This number of Susans have previously been as youth players by Katarina Gutierrez and Melissa Orton',
+				answer: 'Kat and Mo to confirm'
+			},
+			{
+				question:
+					'This player is at the top of the leaderboard for the Golden Boot 2019-2020 season',
+				answer: 'Who is Cat or Alynn'
 			}
 		]
 	},
@@ -21,6 +36,31 @@ module.exports = [
 				question:
 					'This individual is crowned a HERO at the infamous 2012 Olympics scoring a hat trick in one of the most epic battles in Canadian Soccer history.',
 				answer: 'Who is Christine Sinclair?'
+			},
+			{
+				question:
+					"This  woman is a six-time FIFA Women's World Player of the Year recipient, international sensation, and currently holds the record for the most goals *ever* in a World Cup",
+				answer: 'Who is Marta?'
+			},
+			{
+				question:
+					'This nation held Olympic Silver in 2012 London followed by a Women’s World Cup 2015',
+				answer: 'What is Japan?'
+			},
+			{
+				question:
+					'This player is one of the legends who became one of the first ( alongside Formiga of Brazil) to play in 6 World Cup tournaments.She won everything she could want that day (World Cup, Golden, Boot, Golden Ball) even after getting cleated in the vagina during the Semi final match',
+				answer: 'Who is Homare Sawa?'
+			},
+			{
+				question:
+					'This individual is the first female footballer to pursue a career in football, straight out of highschool forgoing the common route of playing varsity soccer',
+				answer: 'Who is Jordyn Huitema?'
+			},
+			{
+				question:
+					'Love is in the air with these two rising Canadian stars representing both the men’s and women’s Canadian national teams. *Hint* the couple recently went to Disneyland in Paris. *Hint hint* The professional clubs they play for are Bayern Munich and Paris St. Germain',
+				answer: 'Who are Jordyn Huitema and Alfonso Davies?'
 			}
 		]
 	},
@@ -40,6 +80,15 @@ module.exports = [
 				question:
 					'This Susan individual sent Melissa her *first ever* Taylor Swift song back in 2006',
 				answer: 'Who is Kelsey Blake?'
+			},
+			{
+				question: 'You can’t spell _____ without me',
+				answer: 'What is Melissa?'
+			},
+			{
+				question:
+					'This song was released in 2012, but is not on any of her albums. The music collaboration won Golden Globe- Best Original Song, Grammy Award for Best Country Duo/ Group, and Country Music Association Award for Musical Event of the Year',
+				answer: 'What is Safe and Sound?'
 			}
 		]
 	},
@@ -49,6 +98,15 @@ module.exports = [
 			{
 				question: 'Female cows are called cows. Male cows are called ________',
 				answer: 'What are bulls?'
+			},
+			{
+				question: 'This species of cow are highly considered the cutest cows',
+				answer: 'What are highland cattle?'
+			},
+			{
+				question:
+					'This specific type of beef (wagyu) is known for its pretty price tag and strict rearing methods. Some of which include: giving names not numbers to all the cows, special brushes (not massage- that is a myth) and proper gear for cold weather',
+				answer: 'What is Kobe beef?'
 			}
 		]
 	}
